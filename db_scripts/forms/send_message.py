4c9f3fc39f003421c75220f8b8520f43eb03c40a
yorik410 <m.aleksandr4100@gmail.com>
2024-04-24 22:30:36 +0400

    fix

--- db_scripts/forms/send_message.py
+++ db_scripts/forms/send_message.py
@@ -1,10 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import PasswordField, BooleanField, SubmitField, EmailField, StringField, IntegerField, DateField, \
     TextAreaField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 
 
 class SendMessageForm(FlaskForm):
-    text = TextAreaField('Send message', validators=[DataRequired()])
+    text = TextAreaField('Send message',
+                         validators=[DataRequired(),
+                                     Length(max=3000, message="You can't enter more than 3000 symbols")])
 
     submit = SubmitField('Send >')
